b417bfa46de8db89cc1bbc1542a76d071917f3c1
Hugo Rafael Alves <hugorafael.alves@hotmail.com>
2024-08-20 22:00:08 -0300

    create room
    Create dynamics rooms

---
+++ components/AddDocumentsBtn.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { createDocument } from "@/lib/actions/room.actions"
+import { useRouter } from "next/navigation"
+
+const AddDocumentsBtn = ({ userId, email }: AddDocumentBtnProps) => {
+  const router = useRouter();
+
+  const addDocumentHandler = async () => {
+    try{
+      const room = await createDocument({ userId, email })
+
+      if(room){
+        router.push(`/documents/${room.id}`)
+      }
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  return (
+    <Button type="submit" onClick={addDocumentHandler} 
+      className="gradient-blue flex gap-1 shadow-md">
+        <Image 
+          src="/assets/icons/add.svg"
+          width={24}
+          height={24}
+          alt=""
+        />
+      <p className="hidden sm:block">Start a blank document</p>
+    </Button>
+  )
+}
+
+export default AddDocumentsBtn
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,24 +11,24 @@ import Header from "@/components/Header"
 const CollaborativeRoom = () => {
   return (
     <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-            <div className='collaborative-room'>
-            <Header>
-                <div className="flex w-fit items-center justify-center gap-2">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className='collaborative-room'>
+          <Header>
+              <div className="flex w-fit items-center justify-center gap-2">
                 <p className="document-title">Share</p>
-                </div>
-                
-                <SignedOut>
+              </div>
+              
+              <SignedOut>
                 <SignInButton />
-                </SignedOut>
-                <SignedIn>
+              </SignedOut>
+              <SignedIn>
                 <UserButton />
-                </SignedIn>
-            </Header>
-            
-            <Editor />
-            </div>
-        </ClientSideSuspense>
+              </SignedIn>
+          </Header>
+          
+          <Editor />
+        </div>
+      </ClientSideSuspense>
     </RoomProvider>
   )
 }
